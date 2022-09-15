41456cd03b4e5f020388860bd2cf55db38d5937e
Caue Vieira <cauevcaetano@gmail.com>
2022-09-15 14:58:27 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿String nome;
+int idade;
+
+nome = "Cauê";
+idade = 16;
+
+Console.WriteLine(nome + " " + idade);
+
+if(idade < 18){
+    Console.WriteLine("Você, " + nome + " é menor de idade (" + idade + ")");
+} else {
+    Console.WriteLine("Você, " + nome + " é maior de idade (" + idade + ")");
+}
